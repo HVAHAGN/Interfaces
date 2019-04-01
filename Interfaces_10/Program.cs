b0c9b45636b631453aa11e04ff6967932710735e
Vahagn Hovhannisyan <vhoagamo@gmail.com>
2019-04-02 00:40:04 +0400

    Inheritance from class and interface whose member signatures match.

---
+++ Interfaces_10/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces_10
+{
+    // Inheritance from class and interface whose member signatures match.
+    interface IInterface
+    {
+        void Method();
+    }
+
+    class BaseClass
+    {
+        public void Method()
+        {
+            Console.WriteLine("BaseClass.Method ()");
+        }
+    }
+
+    class DerivedClass : BaseClass, IInterface
+    {
+        // Implementation of the interface is not necessary, because,
+        // method signatures in the class and interface are the same.
+    }
+
+    class program
+    {
+        static void Main()
+        {
+            DerivedClass instance = new DerivedClass();
+            instance.Method();
+
+            IInterface instance1 = instance as IInterface;
+            instance1.Method();
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
+}
