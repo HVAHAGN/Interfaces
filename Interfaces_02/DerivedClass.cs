2a51875326d01b74c087e16c3f81f49b6af980fb
Vahagn Hovhannisyan <vhoagamo@gmail.com>
2019-04-01 23:42:33 +0400

    Implement from severel interfaces

---
+++ Interfaces_02/DerivedClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces_02
+{
+    class DerivedClass : Interface1, Interface2
+    {
+        public void Method1()
+        {
+            Console.WriteLine("Implementation of Method Method1 () from Interface1");
+        }
+
+        public void Method2()
+        {
+            Console.WriteLine("Implementation of Method Method2 () from Interface2");
+        }
+    }
+}
---
+++ Interfaces_02/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Interface1 instance1 = new DerivedClass();
+            Interface2 instance2 = new DerivedClass();
+
+            instance1.Method1();
+            //instance1.Method2();
+
+            //instance2.Method1(); not available
+            instance2.Method2();
+
+            Console.ReadLine();
+        }
+    }
+}
